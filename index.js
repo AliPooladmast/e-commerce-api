49d6cd264fd9f1bd94d8f060d4833bbdfa8bb895
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-08-18 20:43:05 +0430

    connect to mongodb database cluster

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log(err));
 
 app.listen(5000, () => {
   console.log("Backend server is running");
