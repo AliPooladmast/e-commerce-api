3a2d15442242bd402cd0ca53cde9ad3bed31a00b
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-08-19 13:57:51 +0430

    add auth route for registering process

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use(express.json());
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const User = require("../models/User");
+
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(savedUser);
+  }
+});
+
+module.exports = router;
