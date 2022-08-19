23914c9a5b223f7292adf1ea180808e6da621ef4
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-08-19 16:00:25 +0430

    increase security by hashing  password with crypto js

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const cryptoJS = require("crypto-js");
 
 router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: cryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY),
   });
 
   try {
