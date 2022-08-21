fafa2d342cb3463a7dd14648c714de24cc7921d6
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-08-21 15:54:30 +0430

    add validation for token in the request header beforer verifying

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-  const token = req.headers.token.split(" ")[1];
+  const token = req.headers.token?.split(" ")[1];
   if (!token)
     return res.status(401).json("access denied, token is not provided");
 
