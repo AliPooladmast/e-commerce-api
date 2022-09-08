4e820a1a077313fb0d50f39abc2746b98749f396
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-09-08 10:58:36 +0430

    import order route to index

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 const error = require("./middleware/error");
 require("express-async-errors");
 
@@ -21,6 +22,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 app.use(error);
 
 app.listen(process.env.PORT || 5000, () => {
