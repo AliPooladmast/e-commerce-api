3a11f8a97f419b54a2924dd5def3d92edd4cac1a
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-09-08 15:50:52 +0430

    add stripe route for payment process

---
+++ routes/stripe.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+router.post("/payment", async (req, res) => {
+  stripe.charges.create(
+    {
+      source: req.body.tokenId,
+      amount: req.body.amount,
+      currency: "usd",
+    },
+    (stripeErr, stripeRes) => {
+      if (stripeErr) {
+        res.status(500).json(stripeErr);
+      } else {
+        res.json(stripeRes);
+      }
+    }
+  );
+});
+
+module.export = router;
