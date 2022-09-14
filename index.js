911fed8e93c608a29403ae68a235c462af4afb0b
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-09-14 11:47:07 +0430

    exclude and seperate routes on a different file

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const authRoute = require("./routes/auth");
-const userRoute = require("./routes/user");
-const productRoute = require("./routes/product");
-const cartRoute = require("./routes/cart");
-const orderRoute = require("./routes/order");
-const stripeRoute = require("./routes/stripe");
-const error = require("./middleware/error");
 
 dotenv.config();
 
@@ -19,14 +12,7 @@ mongoose
 
 require("./startup/logging")();
 require("./startup/cors")(app);
-app.use(express.json());
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/products", productRoute);
-app.use("/api/carts", cartRoute);
-app.use("/api/orders", orderRoute);
-app.use("/api/checkout", stripeRoute);
-app.use(error);
+require("./startup/routes")(app);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running");
