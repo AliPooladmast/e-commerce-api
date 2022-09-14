dc05b0bbad32392efe516c57ea88a32c5aaebf3f
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-09-15 00:37:06 +0430

    exclude and seperate db module in a new file

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-
 dotenv.config();
-
-mongoose
-  .connect(process.env.DB_URL)
-  .then(() => console.log("Connected to DB"))
-  .catch((err) => console.log(err));
 
 require("./startup/logging")();
 require("./startup/cors")(app);
 require("./startup/routes")(app);
+require("./startup/db")();
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running");
