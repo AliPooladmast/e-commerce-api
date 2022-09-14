022ce07c5d96de6cb180ffc4969c96c0988c64af
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-09-14 21:55:28 +0430

    change the logging startup module according to the new version of winston

--- startup/logging.js
+++ startup/logging.js
@@ -2,14 +2,24 @@ const winston = require("winston");
 require("express-async-errors");
 
 module.exports = function () {
-  winston.handleExceptions(
-    new winston.transports.Console({ colorize: true, prettyPrint: true }),
-    new winston.transports.File({ filename: "uncaughtExceptions.log" })
-  );
+  const { transports, createLogger, format } = winston;
+  const { combine, json, colorize, prettyPrint } = format;
+
+  const logger = createLogger({
+    level: "info",
+    format: combine(json(), colorize(), prettyPrint()),
+    defaultMeta: { service: "user-service" },
+    transports: [
+      new transports.File({ filename: "error.log", level: "error" }),
+      new transports.File({ filename: "info.log", level: "info" }),
+    ],
+    exceptionHandlers: [
+      new transports.File({ filename: "uncaughtExceptions.log" }),
+      new transports.Console(),
+    ],
+  });
 
   process.on("unhandledRejection", (ex) => {
     throw ex;
   });
-
-  winston.add(winston.transports.File, { filename: "logfile.log" });
 };
