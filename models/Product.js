e427709cfc2418f2d1f1d1397d793f3c2ee060a4
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-09-15 16:36:40 +0430

    change size and color types to array on mongoose model

--- models/Product.js
+++ models/Product.js
@@ -6,8 +6,8 @@ const ProductSchema = new mongoose.Schema(
     desc: { type: String, required: true },
     img: { type: String, required: true },
     categories: { type: Array, required: true },
-    size: { type: String },
-    color: { type: String },
+    size: { type: Array },
+    color: { type: Array },
     price: { type: Number, required: true },
   },
   { timestamps: true }
