f2b130efad443ae5083a56e8d9b149413ffd6593
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-09-15 15:25:40 +0430

    attach mongoose verification to user routes that contain id params

--- routes/user.js
+++ routes/user.js
@@ -5,37 +5,50 @@ const {
 const router = require("express").Router();
 const CryptoJS = require("crypto-js");
 const User = require("../models/User");
+const validateObjectId = require("../middleware/validateObjectId");
 
 //Upadate User
-router.put("/:id", verifyTokenAutorize, async (req, res) => {
-  if (req.body.password) {
-    req.body.password = CryptoJS.AES.encrypt(
-      req.body.password,
-      process.env.PASSWORD_SECRET_KEY
-    ).toString();
+router.put(
+  "/:id",
+  [verifyTokenAutorize, validateObjectId],
+  async (req, res) => {
+    if (req.body.password) {
+      req.body.password = CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.PASSWORD_SECRET_KEY
+      ).toString();
+    }
+
+    updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+
+    res.json(updatedUser);
   }
-
-  updatedUser = await User.findByIdAndUpdate(
-    req.params.id,
-    { $set: req.body },
-    { new: true }
-  );
-
-  res.json(updatedUser);
-});
+);
 
 //Delete User
-router.delete("/:id", verifyTokenAutorize, async (req, res) => {
-  await User.findByIdAndDelete(req.params.id);
-  res.json("user has been deleted...");
-});
+router.delete(
+  "/:id",
+  [verifyTokenAutorize, validateObjectId],
+  async (req, res) => {
+    await User.findByIdAndDelete(req.params.id);
+    res.json("user has been deleted...");
+  }
+);
 
 //Get User
-router.get("/find/:id", verifyTokenAdmin, async (req, res) => {
-  const user = await User.findById(req.params.id);
-  const { password, ...other } = user._doc;
-  res.json(other);
-});
+router.get(
+  "/find/:id",
+  [verifyTokenAdmin, validateObjectId],
+  async (req, res) => {
+    const user = await User.findById(req.params.id);
+    const { password, ...other } = user._doc;
+    res.json(other);
+  }
+);
 
 //Get All Users
 router.get("/", verifyTokenAdmin, async (req, res) => {
