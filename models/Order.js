91b234f04c8dd5802c52a239d6884b1b61fe0716
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-10-30 16:21:23 +0330

    add joi object to orders model

--- models/Order.js
+++ models/Order.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Joi = require("joi");
 
 const OrderSchema = new mongoose.Schema(
   {
@@ -9,11 +10,20 @@ const OrderSchema = new mongoose.Schema(
         quantity: { type: Number, default: 1 },
       },
     ],
-    amount: { type: Number, required: true },
+    amount: { type: Number, required: true, min: 1 },
     address: { type: Object, required: true },
     status: { type: String, default: "pending" },
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("Order", OrderSchema);
+const schema = Joi.object({
+  userId: Joi.string().hex().required(),
+  products: Joi.array().required(),
+  amount: Joi.number().min(1).required(),
+  address: Joi.object().required(),
+  status: Joi.string().default("pending"),
+});
+
+module.exports.Order = mongoose.model("Order", OrderSchema);
+module.exports.schema = schema;
