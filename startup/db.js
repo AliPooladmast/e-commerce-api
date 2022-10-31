c4ecbda26f6d2836a23af39d6360900dfdb18b1f
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-10-31 14:13:55 +0330

    change the message of DB connection

--- startup/db.js
+++ startup/db.js
@@ -4,6 +4,6 @@ const winston = require("winston");
 module.exports = function () {
   mongoose
     .connect(process.env.DB_URL)
-    .then(() => winston.info("Connected to DB"))
+    .then(() => winston.info("Backend server is connected to DB"))
     .catch((err) => winston.error(err));
 };
