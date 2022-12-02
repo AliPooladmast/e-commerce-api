022e00c5965119ead1fffb62d159e68c0c90c889
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-12-02 23:45:27 +0330

    fix image validation

--- models/Product.js
+++ models/Product.js
@@ -58,7 +58,7 @@ const ProductSchema = new mongoose.Schema(
 const schema = Joi.object({
   title: Joi.string().min(3).max(50).required().trim(),
   desc: Joi.string().min(3).max(1024),
-  img: Joi.string(),
+  img: Joi.string().min(0),
   categories: Joi.array().max(10).items(Joi.string().min(1).max(20)),
   size: Joi.array().required().max(10).items(Joi.string().min(1).max(5)),
   color: Joi.array().required().max(10).items(Joi.string().min(1).max(20)),
