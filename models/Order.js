62d9c1733d7fb50c2167770f688b295c7f7e0bfd
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-12-11 08:39:04 +0330

    update order model according to stripe

--- models/Order.js
+++ models/Order.js
@@ -4,26 +4,62 @@ const Joi = require("joi");
 const OrderSchema = new mongoose.Schema(
   {
     userId: { type: String, required: true },
-    products: [
-      {
-        productId: { type: String },
-        quantity: { type: Number, default: 1 },
-      },
-    ],
-    amount: { type: Number, required: true, min: 1 },
-    address: { type: Object, required: true },
-    status: { type: String, default: "pending" },
+    products: {
+      type: [
+        {
+          productId: { type: String, required: true },
+          quantity: {
+            type: Number,
+            default: 1,
+            min: 1,
+            max: 10,
+          },
+        },
+      ],
+      required: true,
+      validate: [arrayLimit, "{PATH} exceeds the limit of 100"],
+    },
+    amount: { type: Number, required: true, min: 1, max: 1000 },
+    phone: {
+      type: String,
+      minlength: 5,
+      maxlength: 20,
+      required: true,
+    },
+    address: {
+      type: String,
+      minlength: 5,
+      maxlength: 511,
+      required: true,
+    },
+    status: {
+      type: String,
+      default: "pending",
+      minlength: 1,
+      maxlength: 20,
+    },
   },
   { timestamps: true }
 );
 
 const schema = Joi.object({
-  userId: Joi.string().hex().required(),
-  products: Joi.array().required(),
-  amount: Joi.number().min(1).required(),
-  address: Joi.object().required(),
-  status: Joi.string().default("pending"),
+  products: Joi.array()
+    .required()
+    .min(1)
+    .max(100)
+    .items(
+      Joi.object({
+        productId: Joi.string().hex().required(),
+        quantity: Joi.number().min(1).max(10),
+      })
+    ),
+  phone: Joi.string().min(5).max(20).required(),
+  address: Joi.string().min(5).max(511).required(),
 });
+
+function arrayLimit(val) {
+  return val.length <= 100;
+}
 
 module.exports.Order = mongoose.model("Order", OrderSchema);
 module.exports.schema = schema;
