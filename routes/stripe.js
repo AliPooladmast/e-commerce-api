c569c07eabc878552913bc3f626cb1580fa9c643
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-12-11 08:40:40 +0330

    update stripe to session creation version

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,22 +1,52 @@
 const router = require("express").Router();
 const Stripe = require("stripe");
+const { verifyTokenAutorize } = require("../middleware/verifyToken");
+const { Product } = require("../models/Product");
+const { schema, Order } = require("../models/Order");
+const validateSchema = require("../middleware/validateSchema");
+const validateObjectId = require("../middleware/validateObjectId");
+const stripe = Stripe(process.env.STRIPE_KEY);
 
-router.post("/payment", async (req, res) => {
-  const stripe = Stripe(process.env.STRIPE_KEY);
-  stripe.charges.create(
-    {
-      source: req.body.tokenId,
-      amount: req.body.amount,
-      currency: "usd",
-    },
-    (stripeErr, stripeRes) => {
-      if (stripeErr) {
-        res.status(500).json(stripeErr);
-      } else {
-        res.json(stripeRes);
-      }
+router.post(
+  "/create-session/:id",
+  [validateObjectId, verifyTokenAutorize, validateSchema(schema)],
+  async (req, res) => {
+    const { products } = req.body;
+
+    const productIds = products.map((item) => item.productId);
+
+    const dbProducts = await Product.find({
+      _id: { $in: productIds },
+    });
+
+    const lineItems = dbProducts.reverse().map((item, index) => ({
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: item._doc.title,
+        },
+        unit_amount: Math.round(item._doc.price * 100),
+      },
+      quantity: products[index].quantity,
+    }));
+
+    if (lineItems) {
+      const session = await stripe.checkout.sessions.create({
+        line_items: lineItems,
+        mode: "payment",
+      });
+
+      const newOrder = new Order({
+        ...req.body,
+        userId: req.params.id,
+        amount: session.amount_total,
+        status: session.payment_status,
+      });
+      const savedOrder = await newOrder.save();
+
+      res.json(savedOrder.status);
     }
-  );
-});
+  }
+);
 
 module.exports = router;
